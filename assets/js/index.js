e35536ae60363d0f6d69125859564d10a9ad42de
dmahoney <p!ttp@nth3r>
2016-12-02 14:18:45 -0500

    adding img, js, and css for skeleton of the web page

---
+++ assets/js/index.js
@@ -0,0 +1,38 @@
+function ready(fn) {
+    if (document.readyState != 'loading') {
+        fn();
+    } else if (document.addEventListener) {
+        document.addEventListener('DOMContentLoaded', fn);
+    } else {
+        document.attachEvent('onreadystatechange', function() {
+            if (document.readyState != 'loading')
+                fn();
+        });
+    }
+}
+
+
+function scrollMe() {
+    $("#header-nav").click(function() {
+        $('html,body').animate({
+                scrollTop: $("#navbar-main").offset().top
+            },
+            'slow');
+    });
+
+    $("#about-nav").click(function() {
+        $('html,body').animate({
+                scrollTop: $("#about").offset().top
+            },
+            'slow');
+    });
+
+    $("#contact-nav").click(function() {
+        $('html,body').animate({
+                scrollTop: $("#contact").offset().top
+            },
+            'slow');
+    });
+}
+
+ready(scrollMe);
