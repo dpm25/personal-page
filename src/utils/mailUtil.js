0a1900dd21b41f1c7e7548e10cc4c1df594c8e96
dmahoney <p!ttp@nth3r>
2016-12-20 08:51:07 -0500

    mail util module

---
+++ src/utils/mailUtil.js
@@ -0,0 +1,15 @@
+module.exports = {
+    validateEmail: function(email) {
+        let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    },
+
+    safeTagReplace: function(comments) {
+        var tagsToReplace = {
+            '&': '&amp;',
+            '<': '&lt;',
+            '>': '&gt;'
+        };
+        return comments.replace(/[&<>]/g, tagsToReplace);
+    }
+}
