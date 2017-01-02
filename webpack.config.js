3bb2b54ceae002f35a13e71872e925d854773085
dmahoney <p!ttp@nth3r>
2017-01-02 08:48:51 -0500

    cleaning up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,3 @@
-// module.exports = {
-//     entry: ['./assets/js/country.js'],
-//     output: {
-//         path: './assets/build/',
-//         filename: 'country-bundle.js'
-//     },
-//     module: {
-//         loaders: [{
-//             test: /\.js$/,
-//             exclude: /node_modules/,
-//             loader: "babel-loader"
-//         }]
-//     }
-// }
-
 var config = {
     module: {
         loaders: [{
